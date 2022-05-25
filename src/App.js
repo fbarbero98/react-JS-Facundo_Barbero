99ba7305b69e84693c88dc4c7329b18365c8dc4c
Facundo Barbero <f.barberops@gmail.com>
2022-05-25 18:25:14 -0300

    Creado el navBar

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css' 
-import Navbar  from 'react-bootstrap/Navbar';
-import { NavDropdown, NavLink } from 'react-bootstrap';
-import {Nav} from 'react-bootstrap'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 
 function App() {
   return (
-    <div className='App'>
-    <Navbar bg="dark" variant="dark" position="fixed" expand= "sm">
-      <Navbar.Brand href="#inicio">
-        <img src={logo} width="40 rem" height="40 rem"/>
-        Logo
-      </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-      <Nav>
-        <NavDropdown title="Productos">
-          <NavDropdown.Item href='#productos/item1'> Item 1</NavDropdown.Item>
-          <NavDropdown.Item href='#productos/item2'> Item 2</NavDropdown.Item>
-          <NavDropdown.Item href='#productos/item3'> Item 3</NavDropdown.Item>
-          <NavDropdown.Divider title="ofertas" />
-          <NavDropdown.Item href='#productos/item4'> Item 4</NavDropdown.Item>
-          <NavDropdown.Item href='#productos/item5'> Item 5</NavDropdown.Item>
-        </NavDropdown>
-        <NavLink href='#carrito'>Carrito</NavLink>
-        <NavLink href='#aboutUs'>Sobre Nosotros</NavLink>
-        <NavLink href='#contact'>Contacto</NavLink>
-      </Nav>
-      </Navbar.Collapse>
-
-    </Navbar>
-    <div>
-      un texto
-    </div>
-    </div> );
+    <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#inicio"><img src={logo} width="40 rem" height="40 rem"/>Logo</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#inicio">Inicio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#carrito">Carrito</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#nosotros">Nosotros</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Productos
+              </a>
+              <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                <li><a className="dropdown-item" href="#">Item 1</a></li>
+                <li><a className="dropdown-item" href="#">Item 2</a></li>
+                <li><a className="dropdown-item" href="#">Item 3</a></li>
+                <li className="dropdown-divider"></li>
+                <li><a className="dropdown-item" href="#">Item 4</a></li>
+                <li><a className="dropdown-item" href="#">Item 5</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>);
 }
 
 export default App;
