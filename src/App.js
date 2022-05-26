09df714047d8986518d68e784de81ea783f4d440
Facundo Barbero <f.barberops@gmail.com>
2022-05-25 21:46:15 -0300

    se saco el contenido de 'App.js' y se importo el contenido de 'NavBar.jsx'

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
-import logo from './logo.svg';
+import './components/NavBar.jsx'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css' 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import NavBar from'./components/NavBar.jsx';
+
 
 
 function App() {
   return (
-    <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#inicio"><img src={logo} width="40 rem" height="40 rem"/>Logo</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#inicio">Inicio</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#carrito">Carrito</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#nosotros">Nosotros</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Productos
-              </a>
-              <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                <li><a className="dropdown-item" href="#">Item 1</a></li>
-                <li><a className="dropdown-item" href="#">Item 2</a></li>
-                <li><a className="dropdown-item" href="#">Item 3</a></li>
-                <li className="dropdown-divider"></li>
-                <li><a className="dropdown-item" href="#">Item 4</a></li>
-                <li><a className="dropdown-item" href="#">Item 5</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>);
+<div>
+  <NavBar /> 
+</div>);
 }
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,14 @@
 import logo from '.././logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css' 
+import '.././App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 
-function App() {
+function NavBar() {
   return (
     <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#inicio"><img src={logo} width="40 rem" height="40 rem"/>Logo</a>
+        <a className="navbar-brand" href="#inicio"><img src={logo} width="40 rem" height="40 rem" alt='logo'/>Logo</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -24,16 +24,16 @@ function App() {
               <a className="nav-link" href="#nosotros">Nosotros</a>
             </li>
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className="nav-link dropdown-toggle" href="#nada" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Productos
               </a>
               <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                <li><a className="dropdown-item" href="#">Item 1</a></li>
-                <li><a className="dropdown-item" href="#">Item 2</a></li>
-                <li><a className="dropdown-item" href="#">Item 3</a></li>
+                <li><a className="dropdown-item" href="#item1">Item 1</a></li>
+                <li><a className="dropdown-item" href="#item2">Item 2</a></li>
+                <li><a className="dropdown-item" href="#item3">Item 3</a></li>
                 <li className="dropdown-divider"></li>
-                <li><a className="dropdown-item" href="#">Item 4</a></li>
-                <li><a className="dropdown-item" href="#">Item 5</a></li>
+                <li><a className="dropdown-item" href="#item4">Item 4</a></li>
+                <li><a className="dropdown-item" href="#item5">Item 5</a></li>
               </ul>
             </li>
           </ul>
@@ -42,4 +42,4 @@ function App() {
     </nav>);
 }
 
-export default App;
+export default NavBar;
