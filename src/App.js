fe2b63e4bcdd656edda6290351514e264f5cd824
Facundo Barbero <f.barberops@gmail.com>
2022-05-25 21:49:02 -0300

    se sacan los imports de bootstrap css y js en 'App.js'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './components/NavBar.jsx'
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css' 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import NavBar from'./components/NavBar.jsx';
 
 
