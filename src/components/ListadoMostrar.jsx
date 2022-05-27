300f9e95e148a9119451849753d75406aad76de3
Facundo Barbero <f.barberops@gmail.com>
2022-05-27 16:57:56 -0300

    solucionados los problemas del ejemplo

--- src/components/ListadoMostrar.jsx
+++ src/components/ListadoMostrar.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 
+
 export default function ListadoMostrar({ alumnos }) {
+  console.log(alumnos)
+  return (
+    <>
+      {alumnos.map((alumno, index) => {
+        const { nombre, edad } = alumno
+        return <p key={index}>Nombre del alumno : {nombre}, Edad del alumno : {edad}</p>
+      })}
+    </>
+  );
+}
+/*export default function ListadoMostrar({ alumnos }) {
   const { nombre, edad } = alumnos
   console.log(alumnos)
   return (
     <>
-      {alumnos.map((alumno) => (
-        <p>Nombre del alumno :{nombre}, Edad del alumno :{edad}</p>
-      ))}
+      {alumnos.map((alumno) => {
+      const {nombre, edad} = alumno
+
+      <p>"Nombre del alumno :{nombre}, Edad del alumno :{edad}"</p>
+})}
     </>
   );
 }
+*/
