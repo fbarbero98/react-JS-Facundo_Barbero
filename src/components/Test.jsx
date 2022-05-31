93cabb210edcc363300d32f0d211cc7e7dc17047
Facundo Barbero <f.barberops@gmail.com>
2022-05-30 21:25:59 -0300

    Se hizo el ejemplo pero con los hooks

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Test({desde}) {
-    let x = 0
+export default function Test({ desde }) {
+    /*let x = 0  */
     console.log("render")
+    const [x, setX] = useState(0); //Este es el hook, devuelve el valor y una funcion para poder cambiar el valor
     return (
 
+        <>
+            <div style={{ border: "1px solid red", height: "200px" }}>
+                DESDE = {x}
 
-    <> <div style={{ border: "1px solid red" , height: "200px" }}>
-        DESDE = {x}
-
-        <button onClick={() => {
-        x = x + 1
-        console.log(x)
-        }}> SUMAR</button>
-        </div></>
-  )
+                <button onClick={() => {
+                    // x = x + 1  (esta linea ya no va);
+                    setX(x + 1);
+                    console.log(x)
+                }}> SUMAR</button>
+            </div>
+        </>
+    )
 }
