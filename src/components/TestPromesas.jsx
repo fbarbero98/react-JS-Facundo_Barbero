79868aa9bed980e70f0a7f971554dbb49999377d
Facundo Barbero <f.barberops@gmail.com>
2022-06-01 21:17:55 -0300

    Ejemplo solo con resolve

--- src/components/TestPromesas.jsx
+++ src/components/TestPromesas.jsx
@@ -9,13 +9,20 @@ export default function TestPromesas() {
         const pagara = new Promise ((resolve , reject) => { //Esta es la forma de hacer una promesa
         setTimeout(() => {
             
-            resolve('Salio bien la promesa')
+            resolve('Salio bien la promesa') //Esto devuelve si la promesa se cumple
         }, 2000);
         })
-        console.log(pagara)
+        console.log(pagara) //Este console.log se va a ver en pending xq no se completo todavia
         setTimeout(() => {
-            console.log(pagara)
-        }, 3000);
+            console.log(pagara) //Este console.log se va a ver como fulfilled
+        }, 6000); 
+
+
+        pagara.then((result) =>{ //Cuando se resuelva, fijate que resultado hay y mostra
+            
+            console.log("El console.log de 'result' es " + result); //Se ve aca lo que dice en el resolve
+            console.log("El console.log de 'pagara' es " + pagara) //se ve aca el object promise (Que seria la promesa)
+        }) 
     }, []) //Esto es para que solo se ejecute una sola vez
 
 
