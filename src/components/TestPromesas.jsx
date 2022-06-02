dbe978e07482679d35160a6a8ee06824dcafe1ae
Facundo Barbero <f.barberops@gmail.com>
2022-06-01 22:01:43 -0300

    ejemplo con promesas then y catch

--- src/components/TestPromesas.jsx
+++ src/components/TestPromesas.jsx
@@ -1,35 +1,49 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function TestPromesas() {
 
     //no suelto codigo xq se renderiza muchas veces, asi que le meto un ciclo de vida.
 
+
+    const [loading, setLoading] = useState();
+    const [error, setError] = useState();
+    const [resultado, setResultado] = useState()
+
+
     useEffect(() => { //Uso el useEffect xq estoy seguro que se va a ejecutar una sola vez (la primera)
 
-        const pagara = new Promise ((resolve , reject) => { //Esta es la forma de hacer una promesa
-        setTimeout(() => {
-            
-            reject('Salio mal la promesa') //Esto devuelve si la promesa se cumple
-        }, 2000);
+        setLoading(true)
+        setError(false)
+        setResultado()
+        const pagara = new Promise((resolve, reject) => { //Esta es la forma de hacer una promesa
+            setTimeout(() => {
+
+                resolve('Salio bien la promesa') //Esto devuelve si la promesa se cumple
+                reject('Salio mal la promesa') //Esto devuelve si la promesa se cumple
+            }, 5000);
         })
 
 
         pagara
-        
-        .then((result) =>{
-            console.log(result) //No se ve porque solo hay un reject
-        })
-        
-        .catch((error) =>{ //Cuando se resuelva, fijate que resultado hay y mostra
-            
-            console.log("El console.log de 'catch' es " + error); //Se ve aca lo que dice en el reject
-            console.log("El console.log de 'pagara' es " + pagara) //se ve aca el object promise (Que seria la promesa)
-        }) 
+            .then((result) => {
+                setResultado(result);
+                setLoading(false)
+                console.log('entro el then');
+            })
+            .catch((error) => { //Cuando se resuelva, fijate que resultado hay y mostra
+                setError(true);
+                setLoading(false);
+                console.log(error);
+                console.log('entro el catch')
+            })
     }, []) //Esto es para que solo se ejecute una sola vez
 
 
     return (
         <>
+            <div>{loading && 'Loading...'}</div>
+            <div>{resultado && 'Salio bien'}</div>
+            <div>{error && 'hubo un error'}</div>
         </>
     )
 }
