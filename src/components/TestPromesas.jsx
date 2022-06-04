02b2125c17a9952b21af2a537a9ef738ebea43f9
Facundo Barbero <f.barberops@gmail.com>
2022-06-04 10:40:36 -0300

    Commit

--- src/components/TestPromesas.jsx
+++ src/components/TestPromesas.jsx
@@ -1,51 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function TestPromesas() {
+  //no suelto codigo xq se renderiza muchas veces, asi que le meto un ciclo de vida.
 
-    //no suelto codigo xq se renderiza muchas veces, asi que le meto un ciclo de vida.
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState();
+  const [resultado, setResultado] = useState();
 
+  useEffect(() => {
+    //Uso el useEffect xq estoy seguro que se va a ejecutar una sola vez (la primera)
 
-    const [loading, setLoading] = useState();
-    const [error, setError] = useState();
-    const [resultado, setResultado] = useState()
+    setLoading(true);
+    setError(false);
+    setResultado([]);
+    const pagara = new Promise((resolve, reject) => {
+      //Esta es la forma de hacer una promesa
+      setTimeout(() => {
+        resolve("Salio bien la promesa"); //Esto devuelve si la promesa se cumple
+        reject("Salio mal la promesa"); //Esto devuelve si la promesa se cumple
+      }, 5000);
+    });
 
+    pagara
+      .then((result) => {
+        setResultado(result);
+        console.log("entro el then");
+      })
+      .catch((error) => {
+        //Cuando se resuelva, fijate que resultado hay y mostra
+        setError(true);
+        setLoading(false);
+        console.log(error);
+        console.log("entro el catch");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []); //Esto es para que solo se ejecute una sola vez
 
-    useEffect(() => { //Uso el useEffect xq estoy seguro que se va a ejecutar una sola vez (la primera)
-
-        setLoading(true)
-        setError(false)
-        setResultado()
-        const pagara = new Promise((resolve, reject) => { //Esta es la forma de hacer una promesa
-            setTimeout(() => {
-
-                resolve('Salio bien la promesa') //Esto devuelve si la promesa se cumple
-                reject('Salio mal la promesa') //Esto devuelve si la promesa se cumple
-            }, 5000);
-        })
-
-
-        pagara
-            .then((result) => {
-                setResultado(result);
-                console.log('entro el then');
-            })
-            .catch((error) => { //Cuando se resuelva, fijate que resultado hay y mostra
-                setError(true);
-                setLoading(false);
-                console.log(error);
-                console.log('entro el catch')
-            })
-            .finally(() =>{
-                setLoading(false)
-            })
-    }, []) //Esto es para que solo se ejecute una sola vez
-
-
-    return (
-        <>
-            <div>{loading && 'Loading...'}</div>
-            <div>{resultado && 'Salio bien'}</div>
-            <div>{error && 'hubo un error'}</div>
-        </>
-    )
+  return (
+    <>
+      <div>{loading && "Loading..."}</div>
+      <div>{resultado && "Salio bien"}</div>
+      <div>{error && "hubo un error"}</div>
+    </>
+  );
 }
