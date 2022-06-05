6ad8f5d6153b6e9f08adec911840c78f4da7098e
Facundo Barbero <f.barberops@gmail.com>
2022-06-05 18:49:31 -0300

    Se añadio el json con los productos y la carpeta con sus imagenes

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from 'react'
 
 export default function ItemListContainer({ greeting, tutora, mensaje }) {
 
-  const [personajes, setPersonajes] = useState([])
+  const [productos, setProductos] = useState(["inicial"])
 
   useEffect(() => {
-    fetch("https://www.freetogame.com/api/games")
-      .then(res => res.json())
-      .catch(error => console.error("Error: ", error))
-      .then(res => setPersonajes(res.results))
+    fetch('./products/products.json')
+      .then(res => JSON.stringify(res))
+      .then(res => console.log(res))
+      .catch(error => console.error(error))
   },[])
-console.log(personajes)
+console.log(productos)
 
 
   return (
