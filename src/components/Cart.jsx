373daa82e6ccd9fd03c65bf5389cbc1d67b143ec
Facundo Barbero <95389207+fbarbero98@users.noreply.github.com>
2022-06-27 17:20:08 -0300

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -22,7 +22,7 @@ export default function Cart() {
               <th scope="col">Total</th>
         </tr>
         {cart.map((item, index) => {return (<tr key={index}>
-            <td className='text-center'>{item.name}</td>
+            <td className='text-center'>{item.name}</td> //Poner el src='' para poder ver la imagen.
             <td>{item.quantity}</td>
             <td>$ {item.precio}</td>
             <td><button onClick={() => deleteItem(item.id)} type="button" className="btn btn-danger btn-sm">Eliminar</button></td>
