313e69e172976a2a8ee4acfcc6db71da85717c15
Facundo Barbero <f.barberops@gmail.com>
2022-06-27 20:44:16 -0300

    Suma imagen a la tabla

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -16,13 +16,15 @@ export default function Cart() {
         <thead>
         <tr>
               <th scope="col">Item</th>
+              <th scope='col'>Imagen</th>
               <th scope="col">Cantidad</th>
               <th scope="col">Precio</th>
               <th scope="col">Eliminar</th>
               <th scope="col">Total</th>
         </tr>
         {cart.map((item, index) => {return (<tr key={index}>
-            <td className='text-center'>{item.name}</td>
+            <td>{item.name}</td>
+            <td><img width="200" height="100"src={item.imagen}></img></td>
             <td>{item.quantity}</td>
             <td>$ {item.precio}</td>
             <td><button onClick={() => deleteItem(item.id)} type="button" className="btn btn-danger btn-sm">Eliminar</button></td>
@@ -33,7 +35,7 @@ export default function Cart() {
          <thead>
           <tr className="table-success m-2">
             <th scope='row'colSpan={3}> PRECIO TOTAL: </th>
-              <td colSpan={2}> ${getItemPrice()}</td>
+              <td colSpan={3}> ${getItemPrice()}</td>
           </tr>
          </thead>
 
