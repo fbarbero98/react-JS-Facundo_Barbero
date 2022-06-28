773a65a21553bd17de6e6bcfdfdceb714a27bd66
Facundo Barbero <f.barberops@gmail.com>
2022-06-28 12:22:16 -0300

    Se instalo firebase, se importaron los metodos, se hizo el getDocs de la base de datos para reemplazar el json

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,50 +1,84 @@
 import React, { useEffect, useState } from "react";
 import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
-
-
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
 
 export default function ItemListContainer({ props }) {
-  const [productos, setProductos] = useState([]);  //Este es el estado de los productos, el cual es un array vacio hasta que se complete el fetch
+  const [productos, setProductos] = useState([]); //Este es el estado de los productos, el cual es un array vacio hasta que se complete el fetch
   const [loading, setLoading] = useState(false); //Este es un loading, el cual esta para que se muestre mientras se hace el fetch, este loading cuando se hace el useEffect, pasa a estar en true.
-  const {idCategory} = useParams();
-  console.log(idCategory)
-
+  const { idCategory } = useParams();
+  console.log(idCategory);
 
   useEffect(() => {
     setProductos([]);
     setLoading(true); //Hacemos que el loading este en "true"
-    const getProductos = () => {
-      fetch(
-        "../../games.json" //Aca esta el fetch, el cual recopila datos de un json
-      )
-        .then((res) => res.json()) //res es el "result" de la promesa, el cual hay que transformar en un json
-        .then((data) => (!idCategory) ? setProductos(data) : setProductos(data.filter(prod => prod.category == idCategory))) //Si el id del useParams == undefined, entonces set productos son todos los productos, si tiene un id entonces los productos se muestran solo los que la categoria coincida con el id
-        .catch((error) => console.error("Error:", error))
+
+    /* 
+    // const getProductos = () => {
+    //   fetch(
+    //     "../../games.json" //Aca esta el fetch, el cual recopila datos de un json
+    //   )
+    //     .then((res) => res.json()) //res es el "result" de la promesa, el cual hay que transformar en un json
+    //     .then((data) => (!idCategory) ? setProductos(data) : setProductos(data.filter(prod => prod.category == idCategory))) //Si el id del useParams == undefined, entonces set productos son todos los productos, si tiene un id entonces los productos se muestran solo los que la categoria coincida con el id
+    //     .catch((error) => console.error("Error:", error))
+    //     .finally(setLoading(false)); 
+    */
+
+    const db = getFirestore(); //Esto es la conexion a la base de datos, dentro de la const db (funciona xq ya importamos todo)
+    const productsCollection = collection(db, "productos"); //al metodo collection le pasamos la dataBase que queremos, y la coleccion que queremos de esa DB (productos en este caso)
+
+    if (idCategory) { //Aca hacemos un if, Si el idCategory del useParams == undefined, entonces set productos son todos los productos, si tiene un idCategory entonces se muestran solo los productos que la category coincida con el idCategory
+      const q = query(productsCollection, where("category", "==", idCategory));
+      getDocs(q)
+        .then((snapshot) => {
+          setProductos(
+            snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+          );
+          
+        })
+        .catch((error) => console.error("Error: ", error))
         .finally(setLoading(false)); //El finally lo que hace es que cuando se termine la promesa, el "loading" vuelva a false
-        
-    };
-    setTimeout(() => {
-      // para simular MAS el delay del servidor, ya que el json responde rapido, SACAR esta linea para un uso real.
-      getProductos()
-    }, 2500);
+    } else {
+      getDocs(productsCollection)
+        .then((snapshot) => {
+          //el metodo getDocs nos trae una coleccion (la que guardamos en collection), y es una promesa, x lo que se le pone un .then
+          //La promesa devuelve muchos datos, de esos datos solo necesitamos el "data" y el "ID" que estan en .docs
+          setProductos(
+            snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+          );
+          //Hacemos un map a cada doc de "docs", y por cada uno hacemos un "spread" para sumar el ID dentro del objeto.
+        })
+        .catch((error) => console.error("Error: ", error))
+        .finally(setLoading(false)); //El finally lo que hace es que cuando se termine la promesa, el "loading" vuelva a false
+    }
+    /* 
+  // };
+     //setTimeout(() => {
+       // para simular MAS el delay del servidor, ya que el json responde rapido, SACAR esta linea para un uso real.
+       //getProductos()
+     //}, 2500); 
+    */
   }, [idCategory]); //Cada vez que el id de la ruta cambie, se ejecuta el useEffect
-
-  
-
-
 
   return (
     <>
-    {loading 
-    ? 
-    <div className="d-flex align-items-center justify-content-center">Loading Videogames...</div>
-    :
-    <ItemList productos={productos} />}   {/*si el loading es true, se muestra el div, sino se ejecuta el ItemList */}
+      {loading ? (
+        <div className="d-flex align-items-center justify-content-center">
+          Loading Videogames...
+        </div>
+      ) : (
+        <ItemList productos={productos} />
+      )}{" "}
+      {/*si el loading es true, se muestra el div, sino se ejecuta el ItemList */}
     </>
   );
 }
-
 
 //Aca mientras el "loading" sea true, se muestra ese div. DOM dinamico
 //Tambien, a ItemList, se le pasa como parametro el valor del estado "productos"
--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDlmfTlG1mwtgtYftj7wHkzKS5p5k00XM0",
+  authDomain: "app-de-facundo-barbero.firebaseapp.com",
+  projectId: "app-de-facundo-barbero",
+  storageBucket: "app-de-facundo-barbero.appspot.com",
+  messagingSenderId: "150983056571",
+  appId: "1:150983056571:web:7d6ef3830852dd56f3dd9e",
+  measurementId: "G-MJ3FVGH1DM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
