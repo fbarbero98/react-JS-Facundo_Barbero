017d548abe9296807d2cadfdc77724a4ba70d97d
Facundo Barbero <f.barberops@gmail.com>
2022-07-05 12:42:07 -0300

    Se añadio footer y estilos css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="mt-5">
+    <footer className='footer'>
     <p>pie de pagina - &copy; 2022 - desarrollado por: Barbero Facundo.</p>
   </footer>
   )
